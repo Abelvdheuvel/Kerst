780a3b6a7b4680f306b1072289f9186ef1da9196
abelvandenheuvel <abelvdheuvel@gmail.com>
2023-12-23 20:02:42 +0100

    final

--- Kerst.py
+++ Kerst.py
@@ -1,8 +1,53 @@
 import streamlit as st 
+import folium
+import streamlit_folium
+
+from haversine import haversine
+from point_dict import points
+
+CENTER = (52.0968641, 5.1209436)  # Noorderstraat 24, Netherlands
+
+def distance_info():
+    total_km = 0
+    for point in points:
+        distance = haversine(CENTER, point['coordinates'])
+        total_km += distance
+    
+    avg_distance = total_km / len(points)
+
+    return round(total_km), round(avg_distance)
 
 def main():
-    st.title('Kerst 2023')
+    st.title('🌲 Kerst 2023 🌲')
+
+    total_distance, avg_distance = distance_info()
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric('Totale Afstand', str(total_distance) + 'km')
+
+    with col2:
+        st.metric('Gemiddelde Afstand', str(avg_distance) + 'km')
+        
+
+    m = folium.Map(location=[CENTER[0], CENTER[1]], zoom_start=8, tiles='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', attr='CartoDB.Voyager')
+
+    for point in points:
+        folium.Marker(location=[point['coordinates'][0], point['coordinates'][1]], 
+                    popup=point['name'], tooltip=point['name'], icon=folium.Icon(color='darkred', icon='map-pin')).add_to(m)
+        
+    st_data = streamlit_folium.st_folium(m, width=725)
+
+    total_km = 0
+    for point in points:
+        distance = haversine(CENTER, point['coordinates'])
+        total_km += distance
+        name = point['name']
+        st.write(f'{name}, {distance:.2f} km')
+
 
 
 if __name__ == '__main__':
     main()
+    
---
+++ point_dict.py
@@ -0,0 +1,11 @@
+points = [
+        {"name": "De Molenwinkel: Bloem ", "coordinates": (51.9426953, 6.6171618)}, 
+        {"name": "Bigoli: Amarena Kersen", "coordinates": (52.0927034, 5.1192163)},
+        {"name": "De Moestuinwinkel: Eieren", "coordinates": (52.07735824584961, 5.1501617431640625)},
+        {"name": "De Hondspol: Melk", "coordinates": (52.03739547729492,5.285699367523193)},
+        {"name": "Ron Visch Notenspecialist: Pistache noten en Rozijnen", "coordinates": (51.8297742,4.9866212)},
+        {"name": "Utrechtse Notenhandel: Amandelen", "coordinates": (52.0930899,5.1147338)},
+        {"name": "Weeribben zuivel: Boter, Melk, Room", "coordinates": (52.7494953,5.9583366)},
+        {"name": "Biohof: Eieren", "coordinates": (52.1554464,5.5044702)},
+        {"name": "Ekoplaze: Chocolade", "coordinates": (52.09584045410156,5.108526706695557)},        
+    ]
